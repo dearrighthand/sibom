827ca386fe7b77f51f23c1bbe9b2944b8d03645d
dearrighthand <dearrighthand@gmail.com>
2026-02-10 23:19:18 +0900

    backend cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://sibom-web.vercel.app',
+      'https://sibom-api.vercel.app',
+      'capacitor://localhost',
+      'http://localhost',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3001);
